f0cf5c358b5f74beee4b7ea77a04d766de7bf2ba
Tanvir ahmed <tanvirahmedshihab36@gmail.com>
2024-08-10 23:21:28 +0600

    Initial commit

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -15,7 +15,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order for frontend
 const placeOrder = async (req,res) =>{
 
-    const frontend_url = "http://localhost:5173/"
+    const frontend_url = "https://nagar-poribahan.netlify.app/"
 
 
     try {
@@ -53,7 +53,7 @@ const placeOrder = async (req,res) =>{
         const session = await stripe.checkout.sessions.create({
             line_items:line_items,
             mode:'payment',
-            success_url:`${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+            success_url:`${https://nagar-poribahan.netlify.app/end_url}/verify?success=true&orderId=${newOrder._id}`,
             cancel_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
         })
 
