bbe269f72a44f86ba72c0a3f27cd8e69aaa5cbc2
Tanvir ahmed <tanvirahmedshihab36@gmail.com>
2024-08-11 00:04:49 +0600

    Fixed orderController template literal and other issues

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -45,7 +45,7 @@ const placeOrder = async (req,res) =>{
                 product_data:{
                     name:"Service Charges"
                 },
-                unit_amount:1*0*1
+                unit_amount:1*1*1
             },
             quantity:1
         })
@@ -53,7 +53,7 @@ const placeOrder = async (req,res) =>{
         const session = await stripe.checkout.sessions.create({
             line_items:line_items,
             mode:'payment',
-            success_url:`${https://nagar-poribahan.netlify.app/end_url}/verify?success=true&orderId=${newOrder._id}`,
+            success_url:`${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
             cancel_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
         })
 
