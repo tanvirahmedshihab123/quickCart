97d7301205ec15431a84cdec6c8f79063063ca34
Tanvir ahmed <tanvirahmedshihab36@gmail.com>
2024-09-24 12:59:55 +0600

    Added new pictures to the repository

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://shihabchaiti79:Shihab123045@nagar.yoi3d.mongodb.net/?retryWrites=true&w=majority&appName=nagar').then(()=>console.log("Database Connected"));
+    await mongoose.connect('mongodb+srv://shihabtanvir03:Shihab123045@pataoo.mfiw9b4.mongodb.net/?retryWrites=true&w=majority&appName=pataoo').then(()=>console.log("Database Connected"));
 }
 
 
+//mongodb+srv://shihabchaiti79:Shihab123045@nagar.yoi3d.mongodb.net/?retryWrites=true&w=majority&appName=nagar
